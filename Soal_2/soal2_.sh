9b8b9a640bc2f43f6efdbb439cfa0d0b708824e4
Anggada Putra <47882546+anggadaputra11319@users.noreply.github.com>
2020-02-21 04:20:06 +0700

    Update soal2_.sh

--- Soal_2/soal2_.sh
+++ Soal_2/soal2_.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Random Password"
 random=`< /dev/urandom tr -dc A-Za-z0-9 | fold -w 28 | head -n 1`
+echo "Input nama file"
 read name
 nama=`echo "$name" | tr -dc A-Za-z`
 echo "$random" >> $nama.txt 
