a16e4fb06e1641b5b1130c78fa391fbb5b57dc56
Anggada Putra <47882546+anggadaputra11319@users.noreply.github.com>
2020-02-29 12:24:14 +0700

    Update soal2_.sh

--- Soal_2/soal2_.sh
+++ Soal_2/soal2_.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 random=`< /dev/urandom tr -dc A-Za-z0-9 | fold -w 28 | head -n 1`
-echo "Input nama file"
-read name
+for name in $*
+do
 nama=`echo "$name" | tr -dc A-Za-z`
 echo "$random" >> $nama.txt 
-
+done
